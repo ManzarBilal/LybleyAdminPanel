fd2a87919fe3bcb86d9c610ae00b47c0ca23e0c0
ManzarBilal <manzarbilal@gmail.com>
2023-05-15 16:41:40 +0530

    second

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseUrl= "http://localhost:5000"
+//const baseUrl= "http://localhost:5000"
+const baseUrl="https://lybleyappbackend-production.up.railway.app"
+
 
 const get=(url)=>{
     return axios.get(baseUrl+url)
