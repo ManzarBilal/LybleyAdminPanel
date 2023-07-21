e2ea5177727c29dfabaea6e8052122e5165357b6
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-07-21 12:05:49 +0530

    ffffswswssw

---
+++ src/screens/SpareParts/MyEditBasicinformation.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+ 
+
+function MyEditBasicInformation(props) {
+// const [category,setCategory]=useState();
+const [fault,setFault]=useState("");
+ const fault1=(f)=>{
+     setFault(f);
+     props.onSubmit(f);
+ }
+ const handleIndex=(i)=>{
+      props.onDelete(i);
+      setFault("");
+ }
+let {partName,description,faultType,MRP,bestPrice,productModel,category,partNo,skuNo,length,breadth,height,weight,brandName}=props?.sparePart;
+ 
+let {categories}=props;
+ 
+const categoryById = categories?.filter(p1=>props?.products?.data?.find(f1=>f1?.categoryId===p1?._id))
+let pds=props?.user?.role==="RESELLER" ? props?.products?.data?.filter(f1=>f1?.productCategory===category && f1?.brandName===brandName) : props?.products?.data?.filter(f1=>f1?.productCategory===category && f1?.userId===props?.user?._id);
+ 
+    return (
+        <>
+            <div className="card-header py-3 d-flex justify-content-between bg-transparent border-bottom-0">
+                <h6 className="mb-0 fw-bold ">Basic information</h6>
+            </div>
+            <div className="card-body">
+                <form>
+                    <div className="row g-3 align-items-center">
+                        <div className="col-md-12">
+                            <label className="form-label"> Spare Part Name</label>
+                            <input type="text" className="form-control" name='partName' value={partName} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-6">
+                            <label className="form-label"> Spare Part No.</label>
+                            <input type="text" className="form-control" name='partNo' value={partNo} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-6">
+                            <label className="form-label"> SKU No.</label>
+                            <input type="text" className="form-control" name='skuNo' value={skuNo} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-3">
+                            <label className="form-label"> Length</label>
+                            <input type="text" className="form-control" name='length' value={length} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-3">
+                            <label className="form-label"> Breadth</label>
+                            <input type="text" className="form-control" name='breadth' value={breadth} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-3">
+                            <label className="form-label"> Height</label>
+                            <input type="text" className="form-control" name='height' value={height} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-3">
+                            <label className="form-label"> Weight</label>
+                            <input type="text" className="form-control" name='weight' value={weight} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-12">
+                            <label className="form-label">Description</label>
+                            <textarea type="text" className="form-control" name='description' value={description} onChange={(e) => {props.onChange(e) }} ></textarea>
+                        </div>
+                        <div className="col-md-12">
+                            <label className="form-label"> MRP</label>
+                            <input type="number" className="form-control" name='MRP' value={MRP} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-md-12">
+                            <label className="form-label"> Best Price</label>
+                            <input type="number" className="form-control" name='bestPrice' value={bestPrice} onChange={(e) => {props.onChange(e) }} />
+                        </div>
+                        <div className="col-xl-12 col-lg-12">
+                        <div className="card-body m-0 p-0">
+                            <label className="form-label">Category</label>
+                            <select className="form-select" name='category' value={category} onChange={(e)=>props.onChange(e)} >
+                                <option value="" selected>Choose Category</option>
+                                {categories?.map(c1=>
+                                    <option value={c1.category} >{c1.categoryName}</option>
+                                    )}
+                            </select>
+                        </div>
+                    </div>
+                        <div className="col-xl-12 col-lg-12">
+                        <div className="card-body m-0 p-0">
+                            <label className="form-label">Product Model</label>
+                            <select className="form-select" name='productModel' value={productModel} onChange={(e)=>props.onChange(e)}  >
+                                <option value="" selected>Choose Model</option>
+                               { category && pds?.map(c1=>
+                                    <option value={c1.productName} >{c1.productName}</option>
+                                    )}
+                            </select>
+                             
+                        </div>
+                    </div>
+                        <div className="col-xl-12 col-lg-12">
+                        <div className="card-body m-0 p-0">
+                            <label className="form-label">Fault Type </label>{" "} <span className='text-muted'>(Select max 5)</span>
+                            <select className="form-select" name='fault' value={fault} onChange={(e)=>fault1(e.currentTarget.value)}  >
+                                <option value="" selected>Choose Fault</option>
+                                {props?.faultType?.map(c1=>
+                                    <option value={c1.faultName} >{c1.faultName}</option>
+                                    )}
+                            </select>
+                        </div>
+                        {faultType?.map((f1,i)=><div className='btn btn-sm bg-dark text-white m-1'>{f1} <i className='ms-2 icofont-close-circled' onClick={(e)=> handleIndex(i)}></i></div>)}
+                    </div>
+                        
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
+export default MyEditBasicInformation
---
+++ src/screens/SpareParts/MySparePartImageEdit.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import httpCommon from "../../http-common";
+import { ToastMessage } from '../../components/common/ToastMessage';
+import { useHistory } from 'react-router-dom';
+
+function MySparePartEditImages(props) {
+  const history=useHistory();
+    const handleFileChange = (e) => {
+        const reader = new FileReader();
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+            if (e.target.name === "file") {
+              // setImage(e.target.files[0]);
+               props.onImage(e.target.files[0]);
+               changeImage(e.target.files[0])
+            }
+        }
+    };
+
+    const handleDelete=async (image)=>{
+        try{
+          let response=await httpCommon.patch(`/deleteSparePartImage/${props?.sparePart?._id}`,{img:image});
+          let {data}=response;
+          ToastMessage(data)
+          history.push(`${props?.url}/spareParts-grid`)
+         }catch(err){
+            console.log(err);
+         }
+    }
+
+    const changeImage=async (img)=>{
+          try{
+            const formData=new FormData();
+            formData.append("image",img);
+            let response=await httpCommon.patch(`/uploadSPImage/${props?.sparePart?._id}`,formData);
+            let {data}=response;
+            ToastMessage(data)
+            history.push(`${props?.url}/spareParts-grid`)
+          }catch(err){
+            console.log(err);
+          }
+    }
+
+    let {images}=props?.sparePart;
+  
+    return (<>
+        <div className="card-header py-3 d-flex justify-content-between bg-transparent border-bottom-0">
+            <h6 className="mb-0 fw-bold ">Images</h6>
+        </div>
+        <div className="card-body">
+            <form>
+                <div className="row g-3 align-items-center">
+                    <div className="col-md-12">
+                        <label className="form-label">Product Images Upload</label>
+                        <small className="d-block text-muted mb-2">Only portrait or square images, 2M max and 2000px max-height.</small>
+
+                        <div  className='row' >
+                            <div className='col-2 text-white  d-flex justify-content-center align-items-center' >
+                            <input style={{cursor:"pointer"}}className='bg-dark ps-2 pe-2 pt-5 pb-5' onChange={(e) => handleFileChange(e)} name="file" type="file" accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff, .mp4, .webm, .mp3, awv, .ogg, .glb"></input>
+
+                            </div>
+                           
+                            {
+                                images?.length>0 ?
+                                   images?.map(im1=> <div className='col-3'> 
+                                   <div className='d-flex'><img src={im1
+                                    } alt='' height="200px" width="200px" /> <i style={{fontSize:"30px",cursor:"pointer"}} className='ms-2 icofont-close-circled text-danger' onClick={()=>handleDelete(im1)}></i></div></div>)
+                                    : props?.img ? 
+                                    <img src={URL.createObjectURL(props?.img)
+                                    } alt='' />
+                                    :
+                                    <div className='col-2 '>
+                                        <i className="fa   fa-picture-o" aria-hidden="true"></i>
+                                    </div>
+                            }
+                             
+                            
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </form>
+        </div>
+    </>
+    )
+}
+
+ export default MySparePartEditImages
